eb06144ff7f434b7286d9f01cf543cccb57cda33
Vasyl444 <152529131+Vasyl444@users.noreply.github.com>
2024-05-09 21:02:05 +0200

    js-hw-10

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,5 +1,4 @@
 import iziToast from "izitoast";
-const delayInput = document.querySelector('input[type="number"]');
 const fulfilled = document.querySelector('input[value="fulfilled"]');
 const rejected = document.querySelector('input[value="rejected"]');
 const form = document.querySelector('.form');
@@ -19,98 +18,94 @@ form.addEventListener('submit', (event) => {
 
     const makePromise = () => {     
         return new Promise((resolve, reject) => {
-           let delay = delayInput.value;
+            const delay = document.querySelector('input[type="number"]').value;
             if (result === 'fulfilled') {
-                resolve(delay)                   
+                setTimeout(() => { resolve(delay) }, delay);            
             } else {
-                reject(delay)
+                setTimeout(() => { reject(delay) }, delay);
             };
-            result = '',
-            form.reset()
+            result = '';
+            form.reset();
         });    
     };
     
     makePromise()
-        .then(value => {
-                setTimeout(() => {
-                    iziToast.show({
-                        id: null,
-                        class: 'izi-toast',
-                        title: 'Ok',
-                        titleColor: '#fff',
-                        message: `✅ Fulfilled promise in ${value}ms`,
-                        messageColor: '#fff',
-                        backgroundColor: '#59A10D',
-                        theme: 'light',
-                        color: '#fff',
-                        zindex: 100,
-                        layout: 1,
-                        balloon: false,
-                        close: false,
-                        closeOnEscape: false,
-                        closeOnClick: false,
-                        displayMode: 0, // once, replace
-                        position: 'center', // bottomRight, bottomLeft, topRight, topLeft, topCenter, bottomCenter, center
-                        target: '',
-                        targetFirst: true,
-                        timeout: 5000,
-                        rtl: false,
-                        animateInside: true,
-                        drag: true,
-                        pauseOnHover: true,
-                        resetOnHover: false,
-                        progressBar: true,
-                        progressBarColor: '',
-                        progressBarEasing: 'linear',
-                        overlay: false,
-                        overlayClose: false,
-                        overlayColor: 'rgba(0, 0, 0, 0.6)',
-                        transitionIn: 'fadeInUp',
-                        transitionOut: 'fadeOut',
-                        transitionInMobile: 'fadeInUp',
-                        transitionOutMobile: 'fadeOutDown',
-                    })
-                }, value)
-            })
-        .catch(value => {
-            setTimeout(() => {
-                 iziToast.show({
-                    title: '',
-                    id: null,
-                    class: 'izi-toast',
-                    titleColor: '#fff',
-                    message: `❌ Rejected promise in ${value}ms`,
-                    messageColor: '#fff',
-                    backgroundColor: '#B51B1B',
-                    theme: 'light',
-                    color: '#fff',
-                    zindex: 100,
-                    layout: 1,
-                    balloon: false,
-                    close: false,
-                    closeOnEscape: false,
-                    closeOnClick: false,
-                    displayMode: 0, // once, replace
-                    position: 'center', // bottomRight, bottomLeft, topRight, topLeft, topCenter, bottomCenter, center
-                    target: '',
-                    targetFirst: true,
-                    timeout: 5000,
-                    rtl: false,
-                    animateInside: true,
-                    drag: true,
-                    pauseOnHover: true,
-                    resetOnHover: false,
-                    progressBar: true,
-                    progressBarColor: '',
-                    progressBarEasing: 'linear',
-                    overlay: false,
-                    overlayClose: false,
-                    overlayColor: 'rgba(0, 0, 0, 0.6)',
-                    transitionIn: 'fadeInUp',
-                    transitionOut: 'fadeOut',
-                    transitionInMobile: 'fadeInUp',
-                    transitionOutMobile: 'fadeOutDown',
+        .then(value => {              
+            iziToast.show({
+                id: null,
+                class: 'izi-toast',
+                title: 'Ok',
+                titleColor: '#fff',
+                message: `✅ Fulfilled promise in ${value}ms`,
+                messageColor: '#fff',
+                backgroundColor: '#59A10D',
+                theme: 'light',
+                color: '#fff',
+                zindex: 100,
+                layout: 1,
+                balloon: false,
+                close: false,
+                closeOnEscape: false,
+                closeOnClick: false,
+                displayMode: 0, // once, replace
+                position: 'center', // bottomRight, bottomLeft, topRight, topLeft, topCenter, bottomCenter, center
+                target: '',
+                targetFirst: true,
+                timeout: 5000,
+                rtl: false,
+                animateInside: true,
+                drag: true,
+                pauseOnHover: true,
+                resetOnHover: false,
+                progressBar: true,
+                progressBarColor: '',
+                progressBarEasing: 'linear',
+                overlay: false,
+                overlayClose: false,
+                overlayColor: 'rgba(0, 0, 0, 0.6)',
+                transitionIn: 'fadeInUp',
+                transitionOut: 'fadeOut',
+                transitionInMobile: 'fadeInUp',
+                transitionOutMobile: 'fadeOutDown',
+            });
                 })
-            }, value)
-        });
+        .catch(value => {           
+            iziToast.show({
+                title: '',
+                id: null,
+                class: 'izi-toast',
+                titleColor: '#fff',
+                message: `❌ Rejected promise in ${value}ms`,
+                messageColor: '#fff',
+                backgroundColor: '#B51B1B',
+                theme: 'light',
+                color: '#fff',
+                zindex: 100,
+                layout: 1,
+                balloon: false,
+                close: false,
+                closeOnEscape: false,
+                closeOnClick: false,
+                displayMode: 0, // once, replace
+                position: 'center', // bottomRight, bottomLeft, topRight, topLeft, topCenter, bottomCenter, center
+                target: '',
+                targetFirst: true,
+                timeout: 5000,
+                rtl: false,
+                animateInside: true,
+                drag: true,
+                pauseOnHover: true,
+                resetOnHover: false,
+                progressBar: true,
+                progressBarColor: '',
+                progressBarEasing: 'linear',
+                overlay: false,
+                overlayClose: false,
+                overlayColor: 'rgba(0, 0, 0, 0.6)',
+                transitionIn: 'fadeInUp',
+                transitionOut: 'fadeOut',
+                transitionInMobile: 'fadeInUp',
+                transitionOutMobile: 'fadeOutDown',
+            });
+            });
 })
